1dd02e0d1144dcda5e271ee439e93e05121e0fb0
Your Name Main <you@example.com>
2022-11-24 05:31:03 -0800

    post user data

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
      const categoryCollection =client.db('assignment12Project').collection('category')
-     const productsCollection =client.db('assignment12Project').collection('products')
+     const productsCollection =client.db('assignment12Project').collection('categoryProducts')
+     const bookedItemCollection =client.db('assignment12Project').collection('bookedItem')
+     const usersCollection =client.db('assignment12Project').collection('bookedItem')
       
+    //  category data
      app.get('/category',async (req,res)=>{
         const query ={}
         const result =await categoryCollection.find(query).toArray()
@@ -27,11 +30,27 @@ async function run(){
      })
      app.get('/category/:id',async (req,res)=>{
         const id =req.params.id
-        const filter={category_id: id}
+        const filter ={category_id: id}
         const result =await productsCollection.find(filter).toArray()
         res.send(result)
      })
 
+    //  My Orders Data
+    app.post('/bookedItem',async (req,res)=>{
+        const user =req.body
+        console.log(user);
+        const result =await bookedItemCollection.insertOne(user)
+        res.send(result)
+    })
+
+
+
+    // UserData
+    app.post('/users',async(req,res)=>{
+      const user =req.body;
+      console.log(user);
+    })
+   
     }
     finally{
 
