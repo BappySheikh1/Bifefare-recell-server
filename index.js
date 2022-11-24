4b0b85d91a11bf904ddb3470af19e286e7d0755d
Your Name Main <you@example.com>
2022-11-23 21:04:31 -0800

    Category product added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app =express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000;
+
 
 // mid 
 app.use(cors())
@@ -16,13 +17,20 @@ const uri = process.env.ACCESS_MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
-     const userCollection =client.db('assintment12Project').collection('project')
-
-    //  app.get('/service',async (req,res)=>{
-    //     const query ={}
-    //     const result =await userCollection.find(query).toArray()
-    //     res.send(result)
-    //  })
+     const categoryCollection =client.db('assignment12Project').collection('category')
+     const productsCollection =client.db('assignment12Project').collection('products')
+      
+     app.get('/category',async (req,res)=>{
+        const query ={}
+        const result =await categoryCollection.find(query).toArray()
+        res.send(result)
+     })
+     app.get('/category/:id',async (req,res)=>{
+        const id =req.params.id
+        const filter={category_id: id}
+        const result =await productsCollection.find(filter).toArray()
+        res.send(result)
+     })
 
     }
     finally{
@@ -34,6 +42,7 @@ run().catch(err =>console.log(err))
 app.get('/', (req,res)=>{
     res.send('assignment12 server is running')
 })
+
 app.listen(port,()=>{
     console.log(`assignment12 server is running ${port}`);
 })
